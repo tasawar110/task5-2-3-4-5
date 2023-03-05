7d191dab9124f1d5e0619a9830c46e1863e61dc3
tasawar110 <126941328+tasawar110@users.noreply.github.com>
2023-03-04 22:06:54 -0800

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,26 @@
+#task2 
+a=int(input('enter the ist arument:'))
+b=int(input('enter the ist 2nd argument:'))
+print(a==b)
+print(a!=b)
+#task3 area of plot
+a=int(input('enter the height of plot:'))
+b=int(input('enter the ist width of plot:'))
+print('area of plot:',a*b)
+#task4 BMI calculator
+a=int(input('enter your age:'))
+b=int(input('enter your weight in kg:'))
+c=int(input('enter your height in cm:'))
+print('your BMI is :',b/c)
+#task5
+a=int(input('enter weight into kg:'))
+
+print('converted weight into pounds:',a*2.20)
+#task5 height conversion
+a=int(input('enter height into cm:'))
+print('converted height into inches:',a*0.39)
+#task5 radian to degree
+a=int(input('enter angle into radian:'))
+print('converted angle into degree:',a*57.2958)
+
+
